b6a21372a7ccdb4ed8becfd06e0ae88c8f05fb6c
MOHAN CHENGAL <mchengal@in.ibm.com>
2026-01-25 11:41:49 +0530

    Initial SHRED project setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <>
+            <h1>SHRED</h1>
+            <p>Project initialized with Git and CI/CD pipelines.</p>
+        </>
+    )
+}
+
+export default App
